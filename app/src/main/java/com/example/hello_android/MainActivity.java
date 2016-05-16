5a4ba11dbab9f5abd22d94b9d4e5b04b0c052535
Jianyu.Wang <873869027@qq.com>
2016-05-16 16:38:24 +0800

    Button Add

--- app/src/main/java/com/example/hello_android/MainActivity.java
+++ app/src/main/java/com/example/hello_android/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.hello_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //使用activity_main.xml文件定义界面布局
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickHandler(View view) {
+        TextView tv = (TextView) findViewById(R.id.textView);
+        tv.setText(getString(R.string.hello, new Date().toString()));
     }
 }
