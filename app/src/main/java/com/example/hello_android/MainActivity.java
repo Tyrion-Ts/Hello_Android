63ed53353d8b8dd1f5a1bec4fb458a09f50d2444
Jianyu.Wang <873869027@qq.com>
2016-05-23 21:53:18 +0800

    GridView布局

--- app/src/main/java/com/example/hello_android/MainActivity.java
+++ app/src/main/java/com/example/hello_android/MainActivity.java
@@ -2,21 +2,91 @@ package com.example.hello_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
-import java.util.*;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private GridView gridView;
+    private List<Map<String, Object>> dataList;
+    private SimpleAdapter adapter;
+    // 图片封装为一个数组
+    private int[] icon = { R.drawable.calculator,
+                           R.drawable.ball,
+                           R.drawable.messenger,
+                           R.drawable.map};
+    private String[] iconName = { "计算器", "弹球", "聊天", "地图" };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        //使用activity_main.xml文件定义界面布局
-        setContentView(R.layout.activity_main);
+        //使用grid.xml文件定义界面布局
+        setContentView(R.layout.grid);
+
+        gridView = (GridView) findViewById(R.id.gview);
+        //新建List
+        dataList = new ArrayList<Map<String, Object>>();
+        //获取数据
+        getData();
+        //新建适配器
+        String [] from ={"image","text"};
+        int [] to = {R.id.image, R.id.text};
+        adapter = new SimpleAdapter(this, dataList, R.layout.item, from, to);
+        //配置适配器
+        gridView.setAdapter(adapter);
+
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+            {
+                Log.i("MainActivity", "Position:" + position);
+            }
+        });
+
+        //实现数据对象传递
+        /*Button bn = (Button) findViewById(R.id.bn);
+        bn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                EditText name = (EditText) findViewById(R.id.name);
+                EditText passwd = (EditText) findViewById(R.id.passwd);
+                RadioButton male = (RadioButton) findViewById(R.id.male);
+                String gendar = male.isChecked() ? "男" : "女";
+                Person p = new Person(name.getText().toString(), passwd.getText().toString(), gendar);
+
+                Bundle data = new Bundle();
+                data.putSerializable("person", p);
+                Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                intent.putExtras(data);
+                startActivity(intent);
+            }
+        });*/
     }
 
-    public void clickHandler(View view) {
-        TextView tv = (TextView) findViewById(R.id.textView);
-        tv.setText(getString(R.string.hello, new Date().toString()));
+
+    public List<Map<String, Object>> getData(){
+        //cion和iconName的长度是相同的，这里任选其一都可以
+        for(int i = 0; i < icon.length; i++){
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("image", icon[i]);
+            map.put("text", iconName[i]);
+            dataList.add(map);
+        }
+
+        return dataList;
     }
 }
+
